b1fa020ab2159b34ec81642e387eb93bd2a5b083
devanych <mail@devanych.ru>
2020-11-02 11:10:19 +0300

    Add chmod-var script to bin directory

---
+++ bin/chmod-var.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use FilesystemIterator as Filesystem;
+use RecursiveDirectoryIterator as Directory;
+use RecursiveIteratorIterator as Iterator;
+
+$path = __DIR__ . '/../var';
+$mode = 0777;
+
+if (is_dir($path)) {
+    chmod($path, $mode);
+
+    $iterator = new Iterator(
+        new Directory($path, Filesystem::SKIP_DOTS | Filesystem::CURRENT_AS_PATHNAME),
+        Iterator::SELF_FIRST
+    );
+
+    foreach ($iterator as $item) {
+        chmod($item, $mode);
+    }
+}
+
+exit(0);
