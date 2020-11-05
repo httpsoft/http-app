bd716656aa68905686a290178061baa1984270a5
devanych <mail@devanych.ru>
2020-11-05 11:38:08 +0300

    Add front-controller index.php to public directory

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use HttpSoft\Basis\Application;
+use HttpSoft\ServerRequest\ServerRequestCreator;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = require_once __DIR__ . '/../config/container.php';
+$app = $container->get(Application::class);
+
+(require_once __DIR__ . '/../config/pipeline.php')($app);
+(require_once __DIR__ . '/../config/routes.php')($app);
+
+$app->run(ServerRequestCreator::create());
