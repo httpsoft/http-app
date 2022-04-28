339ed943d6c3074207c58d0b8d5da166283954b3
Evgeniy Zyubin <mail@devanych.ru>
2022-04-28 10:12:36 +0300

    Fix path to composer autoload script in index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use HttpSoft\Basis\Application;
 use HttpSoft\ServerRequest\ServerRequestCreator;
 
-require_once __DIR__ . '/../../../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $container = require_once __DIR__ . '/../config/container.php';
 $app = $container->get(Application::class);
